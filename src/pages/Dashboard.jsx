1c13ea04f838ed8e0eab992ee81457f61d306b7f
Vince Dizon <dizonvinceryan02@gmail.com>
2026-05-01 02:15:14 +0800

    All good

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import api from '../config/axios';
 
-import NormalUserDash from './NOrmalUserDash';
+import NormalUserDash from './NormalUserDash';
 
 function Dashboard() {
   const [role, setRole] = useState(null);
